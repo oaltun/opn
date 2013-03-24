077726e1d40b95c3924858eb3169aec71ee8c673
Oguz Altun <oaltun@epoka.edu.al>
2013-03-25 00:16:35 +0100

    added mathe2/testScripts/testCSxIsland.m. This files shows how to use problem objects with algorithm functions.

--- base/fun/arrayof.m
+++
@@ -1,6 +0,0 @@
-        function arr=arrayof(n,fun,varargin)
-            arr{n}=[];
-            for i=1:n
-                arr{i}=fun(varargin{:});
-            end
-        end
--- base/fun/randintin.m
+++
@@ -1,3 +0,0 @@
-%random integer values between elements of lb and ub.
-function r = randintin(lb,ub)
-r = floor(lb + (ub-lb+1) .* rand(size(ub)));
