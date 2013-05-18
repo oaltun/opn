9354ad8367cef2fc7382b9e124074aedfe6eaff7
Oguz Altun <oaltun@epoka.edu.al>
2013-05-18 10:56:28 +0200

    Visualization is OK. I wish we could interact with the scene while it is being drawn...

--- PyMeta/pymeta/algorithm/pso.py
+++ PyMeta/pymeta/algorithm/pso.py
@@ -17,10 +17,11 @@ class ParticleSwarmOptimization(OptimizationAlgorithm):
         self.name = 'PSO'
         self.npositions = 30
         
-        self.__dict__.update(**kwargs)  # # overwrite defaults with keyword arguments supplied by user
+        self.__dict__.update(**kwargs)  # overwrite defaults with keyword arguments supplied by user
                 
     def search(self):
-        vis = self.problem.visualiser  # #shorter names
+        # ## shorter names to make program easier to read
+        vis = self.problem.visualiser
         f = self.problem.height
         x = self.positions  # name x is preferred in articles
         isdraw = self.isdraw
@@ -30,37 +31,62 @@ class ParticleSwarmOptimization(OptimizationAlgorithm):
         psip = self.psip 
         psig = self.psig 
 
-        if isdraw: vis.pathcolor = rand(0, 1, (1, 3))  # set a random path color
+        # ## initialization
+        
         maxstep = (self.problem.ub - self.problem.lb) / self.maxstepdivisor
         S = x.shape[0]  # number of particles and dimensions 
         v = np.array([randin(-maxstep, maxstep) for _ in xrange(S)])  # initial velocities of the particles
         p = x.copy()  # best known position of each particle. make a hard copy of x
         fp = np.array([f(pos) for pos in x])  # heights of best positions
         idx = fp.argmax()  # # best of the best values: global best value
-
+        colors=[];
+        bestcolor = (1,1,0)
+        def drawbest(pos): vis.drawposition(pos,color=bestcolor,scale_factor=5)
+        if self.isdraw:
+            drawbest(p[idx])
+            for pos in x: 
+                color =tuple(np.random.uniform(0,1,(3,)).tolist()) ; print 'color',color
+                colors.append(color)
+                vis.drawposition(pos,color=color)
+                
+            
+        # ## main loop
         bestpos = p[idx].copy()
         maxhei = fp[idx]
         iteration = 0
         while True:
             iteration = iteration + 1
-            yield (bestpos, maxhei, iteration)  # give control to caller. It will log and decide whether stop.
+            yield (bestpos, maxhei, iteration)  # give control to caller. It will log and decide whether to stop.
             for i in xrange(S):  # #for each particle  
-                xi, vi, pi = x[i], v[i], p[i]  # #better names
+                # #better names
+                xi = x[i].copy() ;print 'xi',xi
+                vi = v[i].copy() ;print 'vi',vi
+                pi = p[i].copy() ;print 'pi',pi
                 
-                rp = rand(0, 1, xi.shape)  # # produce a new position
-                rg = rand(0, 1, xi.shape)
-                vnew = vi * w + (pi - xi) * rp * psip + (bestpos - xi) * rg * psig  # new velocity
+                rp = rand(0, 1, xi.shape) ;print rp # # produce a new position
+                rg = rand(0, 1, xi.shape) ;print rg
+                vnew = vi * w + (pi - xi) * rp * psip + (bestpos - xi) * rg * psig ;print 'vnew',vnew # new velocity
                 x[i] = fixbound(self.problem, self, xi + vnew)  # new position
-                fxi = f(x[i])  # height of the new position
-                v[i] = x[i] - xi  # correct velocity
-                
-                if self.isdraw: vis.drawpath(xi,x[i])
-                
+                #x[i] = xi + vnew ;print 'x[i]',x[i]
+
+                #x[i]=xi+vnew
+                fxi = f(x[i])  ;print 'fxi',fxi # height of the new position
+                v[i] = x[i] - xi ;print 'v[i]',v[i] # correct velocity
+
+                if self.isdraw: 
+                    if not np.all(np.equal(xi,x[i])):
+                        vis.drawpath(xi, x[i],color=colors[i],tube_radius=.3,opacity=.8)  # draw the path
+
                 if fxi > fp[i]:  # # update personal best
                     p[i] = x[i].copy()
                     fp[i] = fxi
                     if fxi > maxhei:  # #update global best
+                        if self.isdraw:
+                            #drawbest(x[i])
+                            vis.drawpath(bestpos,x[i],color=bestcolor,tube_radius=1.5)
                         bestpos = x[i].copy()
-                        maxhei = fxi
-                        yield (bestpos, maxhei, iteration)  # give control to caller. It will log and decide whether stop.
+                        maxhei  = fxi
                         
+                        #print bestpos,maxhei,iteration
+                        yield (bestpos, maxhei, iteration)  # give control to caller. It will log and decide whether to stop.
+
--- PyMeta/pymeta/visualiser/funvis.py
+++ PyMeta/pymeta/visualiser/funvis.py
@@ -9,6 +9,10 @@ from matplotlib.ticker import LinearLocator, FormatStrFormatter
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib as mpl
+from mayavi import mlab
+import time
+from scipy import interpolate
+from tvtk.tools import visual
 
 #mpl.rcParams['legend.fontsize']=10
 
@@ -16,49 +20,99 @@ import matplotlib as mpl
 
 from pymeta.visualiser.visualiserbase import OptimizationVisualiser
 
+
+def getcurveadder():
+    npts = 100 # number of points to sample
+    shape1=np.array([0,.5,.75,.75,.5,0]) #describe your shape in 1d like this
+    
+    #get the adder. This will be used to raise the z coords
+    x=np.arange(shape1.size)
+    xnew = np.linspace(x[0],x[-1] , npts) #sample the x coord
+    tck = interpolate.splrep(x,shape1,s=0) 
+    adder = interpolate.splev(xnew,tck,der=0)
+    adder[0]=adder[-1]=0
+    adder=adder.reshape((-1,1))
+    return adder
+
+def connectbycurve(p1,p2,adder=getcurveadder()):
+    dist = np.sum((p2-p1)**2)**0.5
+    #amp = dist/2.0
+    amp = dist/2
+    npts=adder.size
+    #get a line between points
+    shape3=np.vstack([np.linspace(p1[dim],p2[dim],npts) for dim in xrange(3)]).T
+    #raise the z coordinate
+    shape3[:,-1]=shape3[:,-1]+adder[:,-1]*amp
+    return shape3
+
+
+
 class TwoDFunVisualiser(OptimizationVisualiser):
     def __init__(self, **kwargs):
         OptimizationVisualiser.__init__(self)  # inherit
-        self.fun = None  # #set defaults
+        self.fun = None  # # defaults
         self.lb = None
         self.ub = None
         self.step = None
         self.__dict__.update(**kwargs)  # overwrite
         
-        x = np.arange(self.lb[0], self.ub[0], self.step[0])
-        y = np.arange(self.lb[1], self.ub[1], self.step[1])
-        
-        xx , yy= np.meshgrid(x, y)
-        xxx = xx.reshape((-1,1))
-        yyy = yy.reshape((-1,1))
-        xy=np.hstack((xxx,yyy))
-        zz = np.array([self.fun(pos) for pos in xy]).reshape(xx.shape)
-        self.x=xx
-        self.y=yy
-        self.z=zz
-        self.maxval = np.max(zz)
-        
     def init(self):
-        self.fig = plt.figure()
-        self.ax  = self.fig.gca(projection='3d')
-        self.surf = self.ax.plot_surface(self.x,self.y,self.z, rstride=10,cmap=cm.coolwarm , cstride=10, linewidth=0, antialiased=False)
-        plt.ion()
-        plt.hold(True)
+        ## prepare the surface data:
+#         self.x = np.arange(self.lb[0], self.ub[0], self.step[0])
+#         self.y = np.arange(self.lb[1], self.ub[1], self.step[1])
+        
+        lb=self.lb; ub=self.ub; step=self.step ## shorter names
+        
+        x,y=np.mgrid[lb[0]:ub[0]:step[0],lb[1]:ub[1]:step[1]]
+        def f(a,b): return self.fun(np.array(list((a,b))))
+        shape=x.shape
+        x=x.reshape((-1,1))
+        y=y.reshape((-1,1))
+        z = np.array([f(x[i],y[i]) for i in xrange(x.size)])
+        mins=np.array([np.min(a) for a in (x,y,z)])
+        maxs=np.array([np.max(a) for a in (x,y,z)])
+        self.ranges = np.array([(mins[a],maxs[a]) for a in xrange(3)]).ravel()
+        
+        lens = np.array([100.,100.,20.])
+        self.scaler = lens/(maxs-mins)
+        
+        xsc=x * self.scaler[0]
+        ysc=y * self.scaler[1]
+        zsc=z * self.scaler[2]
+        
+        z=zsc.reshape(shape)
+        x=xsc.reshape(shape)
+        y=ysc.reshape(shape)
+
+        # # draw the surface
+        self.fig = mlab.figure(size=(800, 800),bgcolor=(1,1,1),fgcolor=(0,0,0))
+        visual.set_viewer(self.fig)
+        self.surf = mlab.surf(x, y,z,colormap='gray',opacity=.5)
+        mlab.axes(ranges=self.ranges)
+        mlab.view(0, 0,.25)
+        mlab.outline()
+        
+        #mlab.show()
 
     def drawbest(self,p):
         pass
-    def drawposition(self,p):
-        pass
-    def drawpath(self,p1,p2):
-        z1=self.fun(p1)
-        z2=self.fun(p2)
-        x=[p1[0],p2[0]]
-        y=[p1[1],p2[1]]
-        z=[z1,z2]
-        print 'x y z', x,y,z
-        self.ax.plot(x,y,z)
-        plt.ion()
-        plt.show()
+    
+    def drawposition(self,p,**kwargs):
+        pz = np.hstack((p,self.fun(p))) * self.scaler
+        mlab.points3d(pz[0],pz[1],pz[2],**kwargs)
+    
+    def drawpath(self,p1,p2,**kwargs):
+        if np.all(np.equal(p1,p2)):
+            return
+        pz1 = np.hstack((p1,self.fun(p1))) * self.scaler
+        pz2 = np.hstack((p2,self.fun(p2))) * self.scaler
+        #xyz=np.vstack((pz1,pz2))
+        xyz=connectbycurve(pz1,pz2)
+        mlab.plot3d(xyz[:,0], xyz[:,1], xyz[:,2], figure=self.fig,**kwargs)
+
         if self.isanimate:
-            plt.pause(0.01)
-        
+            time.sleep(0.10)
+    
+            
+    def show(self):
+        mlab.show()
