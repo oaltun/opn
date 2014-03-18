fdb4d50bbf8e5500ae8e393c64fd5e3bc7b75f47
Oguz Altun <oaltun@epoka.edu.al>
2014-03-18 20:48:09 +0100

    bug fix + commenting.

--- PyMeta/pymeta/problem/cec14soo.py
+++ PyMeta/pymeta/problem/cec14soo.py
@@ -23,30 +23,30 @@ class Cec14SOOProblem(pm.OptimizationProblem):
         self.minimize = True
         self.name = self.name + str(self.func_num)
 
-
-#        if (!(nx==2||nx==10||nx==20||nx==30||nx==50||nx==100))
-#        {
-#            printf("\nError: Test functions are only defined for D=2,10,20,30,50,100.\n");
-#        }
+    # only gooddims are supported. See this code in cec14_test_func.c:
+    #        if (!(nx==2||nx==10||nx==20||nx==30||nx==50||nx==100))
+    #        {
+    #            printf("\nError: Test functions are only defined for "
+    #                "D=2,10,20,30,50,100.\n");
+    #        }
     gooddims = [2, 10, 20, 30, 50, 100]
 
-#        if (nx==2&&((func_num>=17&&func_num<=22)||(func_num>=29&&func_num<=30)))
-#        {
-#            printf("\nError: hf01,hf02,hf03,hf04,hf05,hf06,cf07&cf08 are NOT defined for D=2.\n");
-#        }
+    # when number of dimensions is two, following functions are not supported.
+    # See this code in cec14_test_func.c
+    #        if (nx==2&&((func_num>=17&&func_num<=22)||
+    #            (func_num>=29&&func_num<=30)))
+    #        {
+    #            printf("\nError: hf01,hf02,hf03,hf04,hf05,hf06,cf07&cf08 "
+    #                "are NOT defined for D=2.\n");
+    #        }
     badnum2 = [17, 18, 19, 20, 21, 22, 29, 30]
 
-
-
-
-#        nx = self.ub.size
-#        func_num = self.func_num
-#        if not((nx == 2) or (nx == 10) or (nx == 20) or (nx == 30) or (nx == 50) or (nx == 100)):
-#            raise Exception('Error: Test functions are only defined for D or ndims = 2,10,20,30,50,100')
-
-
-
     def costfun(self, x):
+        #x needs to have two dimensions
         x = x.reshape((1, -1))
+
+        # call the wrapped cec function
         v = cec.value(x, self.func_num)
+
+        # cec.value() function returns an array as opposed to a scalar
         return v[0]
