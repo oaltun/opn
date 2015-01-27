a86f515b288bb483510708bd6c21840ba8645da4
Oguz Altun <oaltun@epoka.edu.al>
2015-01-27 10:56:58 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(name='opn',
+      version='0.1.0',
+      description="A global optimization package",
+      long_description="README.txt",
+      author='Oğuz Altun',
+      author_email='oguz211@gmail.com',
+      license='TODO',
+      packages=find_packages(),
+      zip_safe=False,
+      install_requires=[
+			'docutils>=0.12',
+			'pyqt>=4.10.4',
+			'mayavi>=4.3.1',
+			'matplotlib>=1.4.2',
+			'numpy>=1.9.1',
+          ],
+		  
+      )
